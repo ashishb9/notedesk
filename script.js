2bb6ca1104ada0e83baafad3d571af5e152c1308
Ashish B <b9.ashish@gmail.com>
2025-08-01 18:11:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -226,9 +226,8 @@ document.addEventListener("DOMContentLoaded", () => {
       modalContent.innerHTML = note.content;
       bookmarkBtn.dataset.id = noteId;
       bookmarkBtn.innerHTML = `<i class="${note.bookmarked ? 'fas' : 'far'} fa-bookmark"></i> ${note.bookmarked ? 'Remove Bookmark' : 'Bookmark Note'}`;
-      modal.style.display = 'block';
-modal.scrollIntoView({ behavior: 'smooth', block: 'center' });
-document.body.style.overflow = 'hidden';
+      modal.style.display = 'flex';
+document.body.classList.add('modal-open');
     });
 
     // Bookmark button in modal
